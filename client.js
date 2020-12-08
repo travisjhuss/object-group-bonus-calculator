578dc841064dcad2eb19717e56479e60c5f4df68
Travis Huss <travisjhuss@gmail.com>
2020-12-08 14:04:57 -0600

    commented out steps for project

--- client.js
+++ client.js
@@ -41,3 +41,24 @@ const employees = [
 // Ask questions when you don't.
 
 console.log( employees );
+
+
+
+// Steps break down
+// for loop to loop over employee array
+// log each employee
+
+// function that takes each employee object and returns a new object 
+// The 'name' property should contain the employee's name.
+// The 'bonusPercentage' property should contain the bonus percentage the employee is to receive. See section below for calculation instructions.
+// The 'totalCompensation' property should be the adjusted annual compensation (base annual + bonus)
+// The 'totalBonus' should be the employee's total bonus rounded to the nearest dollar.
+
+// Bonus function (switch...case)
+// Those who have a rating of a 2 or below should not receive a bonus.
+// Those who have a rating of a 3 should receive a base bonus of 4% of their base annual income.
+// Those who have a rating of a 4 should receive a base bonus of 6% of their base annual income.
+// Those who have a rating of a 5 should receive a base bonus of 10% of their base annual income.
+// If their employee number is 4 digits long, this means they have been with the company for longer than 15 years, and should receive an additional 5%.
+// However, if their annual income is greater than $65,000, they should have their bonus adjusted down 1%.
+// No bonus can be above 13% or below 0% total.
